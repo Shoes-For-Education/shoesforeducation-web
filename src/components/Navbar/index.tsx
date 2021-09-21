9316da1fc4d9ae0cc16a9432a2e1ee8bf37ca90e
MahitMehta <73650235+MahitMehta@users.noreply.github.com>
2021-09-21 18:05:23 -0400

    Responsive 2

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,11 @@
 import { Typography } from '@material-ui/core';
 import IconButton from '@mui/material/IconButton';
-import React, { ReactChild } from 'react';
+import React, { ReactChild, useState } from 'react';
 import BrandButton from '../BrandButton';
 import { useStyles } from './styles'
 import MenuIcon from "@mui/icons-material/Menu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 type NavbarProps = {
     children: ReactChild;
@@ -12,11 +14,14 @@ type NavbarProps = {
 const Navbar : React.FC<NavbarProps> = ({ children }) => {
     const classes = useStyles();
 
+    const [ showNav, setShowNav ] = useState(false);
+
     return (
         <div className={classes.container}>
             <nav className={classes.nav}>
                 <div className={classes.navIcon}>
                     <IconButton
+                        onClick={() => setShowNav(!showNav)}
                         size="large"
                         edge="start"
                         color="inherit"
@@ -26,26 +31,45 @@ const Navbar : React.FC<NavbarProps> = ({ children }) => {
                         <MenuIcon />
                     </IconButton>
                 </div>
-                <ul className={classes.ul}>
+                <ul 
+                    style={{ transform: showNav ? "translateX(0px)"  : "translateX(-230px)"}}
+                    className={classes.ul}
+                >
                     <li className={classes.li}>
                         <Typography
                             className={classes.liContent}
                         >Request Book</Typography>
+                        <FontAwesomeIcon 
+                            className={classes.arrow}
+                            icon={faChevronRight} 
+                        />
                     </li>
                     <li className={classes.li}>
                         <Typography
                             className={classes.liContent}
                         >Book Choices</Typography>
+                        <FontAwesomeIcon 
+                            className={classes.arrow}
+                            icon={faChevronRight} 
+                        />
                     </li>
                     <li className={classes.li}>
                         <Typography
                             className={classes.liContent}
                         >Donate</Typography>
+                        <FontAwesomeIcon 
+                            className={classes.arrow}
+                            icon={faChevronRight} 
+                        />
                     </li>
                     <li className={classes.li}>
                         <Typography
                             className={classes.liContent}
                         >Login</Typography>
+                        <FontAwesomeIcon 
+                            className={classes.arrow}
+                            icon={faChevronRight} 
+                        />
                     </li>
                     <li className={classes.li}>
                         <BrandButton className={classes.signUp} title="Sign Up" />
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -8,6 +8,7 @@ export const useStyles = makeStyles(theme => ({
         marginTop: 5,
         boxShadow: "0px 5px 5px 5px rgba(0, 0, 0, 0.15)",
         position: 'relative',
+        overflowX: "hidden",
     },
     nav: {
         backgroundColor: "#fff",
@@ -21,53 +22,76 @@ export const useStyles = makeStyles(theme => ({
         display: 'flex',
         paddingLeft: 25,
         paddingRight: 25,
+        "@media(max-width: 950px)": {
+            marginTop: 0,
+            paddingTop: 0,
+        },
     },
     navIcon: {
         display: "none",
+        zIndex: 1,
         "@media(max-width: 950px)": {
             display: "block",
         },
     },
     ul: {
+        transition: "150ms ease",
         display: 'flex',
         marginLeft: 'auto',
         alignItems: 'center',
         "@media(max-width: 950px)": {
             position: 'fixed',
+            transform: "translateX(-230px)",
             left: 0,
             flexDirection: "column",
             height: '100vh',
             backgroundColor: "#fff",
             width: 200,
             top: 0,
-            padding: 0,
-            alignItems: "center",
+            padding: 15,
+            paddingTop: 75,
+            alignItems: "flex-start",
             boxShadow: "0px 0px 5px 2px rgba(0, 0, 0, 0.15)",
         },
     },
     li: {
+        transition: "100ms ease",
         listStyle: 'none',
         paddingRight: 0,
         paddingLeft: 30,
+        display: 'flex',
+        justifyContent: "space-between",
         "&:hover": {
             cursor: "pointer",
+            opacity: 0.2,
         },
         "@media(max-width: 950px)": {
             paddingLeft: 0,
+            marginBottom: 35,
+            width: "100%",
         },
     },
     liContent: {
-        transition: "100ms ease",
         fontSize: 14,
         color: "grey",
         "&:hover": {
             cursor: "pointer",
-            color: "rgba(0, 0, 0, 0.2)"
         }
     },
     signUp: {
         boxShadow: "none",
         width: 85,
         height: 35,
+        "@media(max-width: 950px)": {
+            width: "200px",
+            marginTop: '0px',
+        },
+    },
+    arrow: {
+        display: 'none',
+        color: "grey",
+        "@media(max-width: 950px)": {
+           display: 'block',
+        },
     }
 }));
