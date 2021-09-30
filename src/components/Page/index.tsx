fbfd13f2c3e61ff62d6a01bdacdd15390a1af8b4
Mahit Mehta <mahitmehta@Mahits-MacBook-Air.local>
2021-09-30 19:00:44 -0400

    Adjusted Height

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -27,7 +27,7 @@ const Page : React.FC<PageProps> = ({ children, className }) => {
         <section
             ref={page}
             className={clsx(classes.container, className)}
-            style={{ minHeight: 750, height: pageHeight }}
+            style={{ minHeight: 700, height: pageHeight }}
         >
             { children }
         </section>
