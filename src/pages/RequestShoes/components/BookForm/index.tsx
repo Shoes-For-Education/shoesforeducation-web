718879403e0e17f14b11a6695225e04251814535
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-35.local>
2021-10-07 23:15:15 -0400

    Added Shoe Size Field

---
+++ src/pages/RequestShoes/components/BookForm/index.tsx
@@ -0,0 +1,78 @@
+import { Box, TextField, MenuItem } from "@material-ui/core";
+import { ToggleButtonGroup, ToggleButton } from "@mui/material";
+import clsx from "clsx";
+import { IRequestShoesForm } from "../..";
+import { useStyles } from "../../styles";
+
+type BookFormProps = {
+    books: any[],
+    values: IRequestShoesForm,
+    setValues: (e:IRequestShoesForm) => void,
+}
+
+const BookForm : React.FC<BookFormProps> = ({ books, values, setValues }) => {
+    const handleChange = (type:keyof IRequestShoesForm) => (e:React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [ type ] : e.target.value });
+    }
+    const classes = useStyles();
+
+    const handleProofTypeChange = (_:any, value:any) => {
+        if (!value) return; 
+        setValues({ ...values, "proofType": value });
+    };
+
+    return (
+        <Box className={classes.formType}>
+             <TextField
+                    id="standard-select-book"
+                    select
+                    label="Select"
+                    onChange={handleChange("bookId")}
+                    required={true}
+                    helperText="Choose Book"
+                    variant="standard"
+                    value={values.bookId}
+                    className={clsx(classes.input, classes.select)}
+                    >
+                    {books.map((book:any, index:number) => {
+                        return (
+                            <MenuItem className={classes.selectItem} value={book._id} key={index}>
+                                { book.name }
+                            </MenuItem>
+                        )
+                    })} 
+                </TextField>
+                <ToggleButtonGroup
+                    color="primary"
+                    value={values.proofType}
+                    exclusive
+                    className={classes.proofButtons}
+                    onChange={handleProofTypeChange}
+                    >
+                    <ToggleButton 
+                        className={classes.proofOption} 
+                        value="written">
+                            Written
+                    </ToggleButton>
+                    <ToggleButton 
+                        className={classes.proofOption} 
+                        value="video">
+                            Video
+                    </ToggleButton>
+                </ToggleButtonGroup>
+                <TextField
+                    id="filled-multiline-static"
+                    label="Book Summary"
+                    multiline
+                    rows={4}
+                    onChange={handleChange('summary')}
+                    className={classes.input}
+                    value={values.summary}
+                    variant="outlined"
+                    helperText="What Did You Learn?"
+                />
+        </Box>
+    )
+}
+
+export default BookForm; 
