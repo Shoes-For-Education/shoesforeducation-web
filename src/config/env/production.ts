95fe91e7732aa1e53c514bfe73144e97e4c260f1
Mahit Mehta <mahit.py@gmail.com>
2022-11-24 17:12:08 -0500

    Updated API URL

--- src/config/env/production.ts
+++ src/config/env/production.ts
@@ -1,6 +1,6 @@
 const config = {
     api: {
-        url: "https://staging-api.shoesforeducation.org"
+        url: "https://api.shoesforeducation.org"
     },
     google: {
         clientId: "592260160711-bd7gsbvjq2j6jft0eifed38i4rplpn4g.apps.googleusercontent.com",
