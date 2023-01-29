70b08e79e45d836f6c16a39df969511f52f3329d
mahitmehta <mahit.py@gmail.com>
2023-01-29 15:34:17 -0500

    Footer Style Updates

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import clsx from "clsx";
-import React from "react";
+import React, { MouseEvent } from "react";
 import DonatePopUp from "../DonatePopUp";
 
 interface FooterHeaderProps {
@@ -9,7 +9,7 @@ interface FooterHeaderProps {
 
 const FooterHeader : React.FC<FooterHeaderProps> = ({ children }) => {
     return (
-        <li className="list-none uppercase px-2 text-white my-6 font-bold">
+        <li className="list-none uppercase px-2 text-white my-4 font-bold">
             { children }
         </li>
     )
@@ -22,9 +22,9 @@ interface FooterItemProps extends React.HTMLProps<HTMLAnchorElement> {
 
 const FooterItem : React.FC<FooterItemProps> = ({ href = "/", children, ...props }) => {
     return (
-        <li className={clsx("list-none w-min hover:bg-black transition-all rounded-md px-2 hover:bg-opacity-30 p-1 text-medium-grey my-2 font-medium")}>
+        <li className={clsx("list-none w-min hover:bg-black transition-all rounded-md px-2 hover:bg-opacity-20 p-1 text-medium-grey my-2 font-medium")}>
             <a
-                    href={href}
+                href={href}
                 className="flex whitespace-nowrap no-underline text-[rgba(255,255,255,0.35)] items-center"
                 { ...props }
             >
@@ -36,13 +36,20 @@ const FooterItem : React.FC<FooterItemProps> = ({ href = "/", children, ...props
 
 const Footer = () => {
     const [ donateModalOpen, setDonateModalOpen ] = React.useState(false);
-    const handleOpenDonateModal = () => setDonateModalOpen(true);
+    const handleOpenDonateModal = (e:MouseEvent) => {
+        e.preventDefault();
+        setDonateModalOpen(true);
+    }
 
     return (
         <>
         <DonatePopUp visible={donateModalOpen} handleClose={() => { setDonateModalOpen(!donateModalOpen); }} />
         <footer>
-            <div className="w-full min-h-[100px] pb-3 bg-complementary-grey border-b-[rgba(255,255,255,0.1)] border-b-2 border-solid grid justify-items-center grid-cols-2 md:grid-cols-4 max-w-[1450px]">
+            <div 
+                className={clsx(
+                    "w-full min-h-[100px] pb-3 bg-complementary-grey border-b-[rgba(255,255,255,0.1)] border-b-2 border-solid grid justify-items-center grid-cols-2 md:grid-cols-4 max-w-[1450px]",
+                    "[&>ul]:flex [&>ul]:flex-col md:[&>ul]:items-start [&>ul]:items-center"
+                )}>
                 <ul>
                     <FooterHeader>sitemap</FooterHeader>
                     <FooterItem>Home</FooterItem>
@@ -54,7 +61,7 @@ const Footer = () => {
                 </ul>
                 <ul>
                     <FooterHeader>Support Us</FooterHeader>
-                    <FooterItem onClick={handleOpenDonateModal} href="javascript:void(0)">Donate</FooterItem>
+                    <FooterItem onClick={handleOpenDonateModal}>Donate</FooterItem>
                 </ul>
                 <ul>
                     <FooterHeader>Contact</FooterHeader>
